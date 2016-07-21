7f98f5f393dae7a58544e565a938e1dd4a5da628
nhab <n.habibollahi@yahoo.com>
2016-07-21 17:58:31 +0800

    Update focus.js

--- focus.js
+++ focus.js
@@ -1,10 +1,9 @@
 function a()
 {
-	alert("dgdfg");
-	
+	alert("Test");
 }
 /*__________________________________________________*/
-function radioBox(title,arr)
+function radioBoxes(title,arr)
 {
 	s = "<div>";
 	s = s + "<p>"+title+"</p>";
@@ -16,17 +15,28 @@ function radioBox(title,arr)
 	
 }
 /*__________________________________________________*/
-function line()
+function line(parent)
 {
-	$( "body" ).append("<hr>");
+	var x = document.createElement("HR");
+	 parent = parent || document.body;
+	parent.appendChild(x);
+	return x;
 }
 /*__________________________________________________*/
-function checkBox(title,arr)
+function newLine(parent)
 {
-	s = "<div>";
+	var x = document.createElement("BR");
+	parent = parent || document.body;
+	parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function checkBoxes(title,arr)
+{
+	var s = "<div>";
 	s = s + "<p>"+title+"</p>";
 	for(i=0;i<arr.length;i++)
-		s = s + "<input  name='rb" + i + "' type='checkbox'  />"+arr[i]+"</input>";//
+		s = s + "<input  name='rb" + i + "' type='checkbox'  >"+arr[i]+"</input>";//
 	s=s+"</div>";
 	//console.log(s);
 	$( "body" ).append(s);
@@ -34,27 +44,86 @@ function checkBox(title,arr)
 /*__________________________________________________*/
 function range(title,val,id)
 {
-	s = "<div>";
+	var s = "<div>";
 	s = s + "<p>"+title+"</p>";
 	s = s + "<input type='range' value='"+val+"' id='"+id+"' >";
-	s=s+"</div>";
+	s = s + "</div>";
 	$( "body" ).append(s);
 }
 /*__________________________________________________*/
-function label(title,css)
+function list(parent,css)
 {
-	s=s+"<div class='"+css+"'>";
-	s=s+title;
-	s=s+"</div>";
-	$( "body" ).append(s);
+	var x = document.createElement("UL");
+    x.className =css;
+	parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function listItem(parent,text,css)
+{
+	var x = document.createElement("LI");
+    x.className =css;
+	x.innerHTML=text;
+	parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function Lists(parent,arrItems,css)
+{
+	var  l  = list(parent,css);
+	
+	var len = arrItems.length;
+	for(i=0;i<len;i++)
+		listItem(l,arrItems[i],css);
 	
 }
 /*__________________________________________________*/
-function table()
+function image(parent,source,css)
+{
+	var x = document.createElement("IMG");
+	x.src=source;
+    x.className =css;
+	parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function button(parent,title,css)
+{
+	var x = document.createElement("INPUT");
+	x.setAttribute("type", "button");
+	x.value= title;
+    x.className =css;
+	parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function label(parent,title,css)
+{
+	var x = document.createElement("DIV");
+	x.innerHTML= title;
+    x.className =css;
+	parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function checkBox(parent,title,css)
+{
+	var x = document.createElement("INPUT");
+	x.setAttribute("type", "checkbox");
+	
+    x.className =css;
+	parent.appendChild(x);
+	var t = document.createTextNode(title);
+    document.body.appendChild(t);
+	return x;
+}
+/*__________________________________________________*/
+function table(parent,css)
 { 
 	var x = document.createElement("TABLE");
     //x.setAttribute("id", id);
-    document.body.appendChild(x);
+    x.className =css;
+	parent.appendChild(x);
 	return x;
 }
 /*__________________________________________________*/
@@ -73,3 +142,13 @@ function tableCell(TableRow,content)
     return TableRow.appendChild(z);
 }
 /*__________________________________________________*/
+function addEvent(element, event, fn) {
+    if (element.addEventListener) {
+        element.addEventListener(event, fn, false);
+    } else {
+        element.attachEvent("on" + event, function() {
+            // set the this pointer same as addEventListener when fn is called
+            return(fn.call(element, window.event));   
+        });
+    }
+}
