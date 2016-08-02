99e4c27dc883137f820e3528478bddea1c18bd97
nhab <n.habibollahi@yahoo.com>
2016-08-02 15:29:32 +0800

    Update focus.js

--- focus.js
+++ focus.js
@@ -2,27 +2,104 @@ function a()
 {
 	alert("Test");
 }
+var fnWizPages=[];//an array of functions that create pages
+
 /*__________________________________________________*/
-function radioBoxes(parent,css,title,arr)
+function wizard(parent,fnWizPages,data)
 {
+	 return wizard1(parent,0,fnWizPages,data);
+}
+/*__________________________________________________*/
+function wizard1(parent,i,fnWizPages,data)
+{
+	Clear       ( parent);
+
+	data=fnWizPages[i](parent,data)  ;
+			
+	if(i<fnWizPages.length-1)
+	{
+		btnNext = button (parent,"next","","button");
+		btnNext.addEventListener('clicked', 
+		function (e) {
+			var event = new Event('nextClicked');
+			this.dispatchEvent(event);
+			console.log(e.target);
 	
+			data= wizard1(parent,++i,fnWizPages,data);
+		}
+		,	false );
+	};
+
+	if(i>0)
+	{
+		btnPrev = button (parent,"prev","","button");
+		btnPrev.addEventListener('clicked', 
+			function (e) {
+		var event = new Event('prevClicked');
+		this.dispatchEvent(event);
+		console.log("prev pressed:",data);	
+		data= wizard1(parent,--i,fnWizPages,data);
+		}
+		,	false);
+	};
+	
+	return data;
+}
+/*__________________________________________________*/
+
+function radioBoxes(parent,title,arrItems,css)
+{
 	var x = document.createElement("DIV");
     x.className =css;
 	x.innerHTML=title;
 	parent.appendChild(x);
+	var len=arrItems.length;
+	var sName= "radio"+Math.floor((1 + Math.random()) * 0x1000000).toString();
 	
-	for(i=0;i<arr.length;i++)
-	{
+	for(i=0;i<len;i++)
+	{		
 		var x1 = document.createElement("input");
 		x1.className =css;
 		x1.type="radio";
 		x1.style="display:inline-block;";
+		x1.id="rb"+i;
+		
+		x1.name=sName;
+
+		x1.onchange=function(){
+			if(x1.checked=="checked")
+			{
+				var event = new Event('change');
+				event.selected = x1.id.substring(2);
+				x.dispatchEvent(event);	
+			  	
+					  
+			}
+		};
 		x.appendChild(x1);
-		label(x,arr[i],css);
+		label(x,arrItems[i],css);
 	}
 	
 	return x;
 }
+/*__________________________________________________*/
+
+function radioBoxes_set(radioboxes,indx)
+{
+	var len=radioboxes.children.length;
+	var id="rb"+indx;
+	for(i=0;i<len;i++)
+		if(radioboxes.children[i].id==id)
+			radioboxes.children[i].checked=true;
+
+}
+/*__________________________________________________*/
+// onChangeFunction has a parameter which contain every details related to the event
+function radioBoxes_OnChange(radioboxes,onChangeFunction)
+{
+	radioboxes.addEventListener('change',onChangeFunction,false);
+}
+
 /*__________________________________________________*/
 function line(parent)
 {
@@ -40,49 +117,44 @@ function newLine(parent)
 	return x;
 }
 /*__________________________________________________*/
-function checkBoxes(parent,css,title,arr)
+function checkBoxes(parent,title,arrItems,css)
 {
 	var x = document.createElement("DIV");
     x.className =css;
 	x.innerHTML=title;
 	parent.appendChild(x);
 	
-	for(i=0;i<arr.length;i++)
+	for(i=0;i<arrItems.length;i++)
 	{
 		var x1 = document.createElement("input");
 		x1.className =css;
 		x1.type="checkbox";
 		x1.style="display:inline-block;";
 		x.appendChild(x1);
-		label(x,arr[i],css);
+		label(x,arrItems[i],css);
 	}
 	
 	return x;
 }
 /*__________________________________________________*/
 var objVal;
-function range(parent,title,css,min,max,val)
+function range(parent,title,arrValues ,css)
 {
 	var x = document.createElement("INPUT");
 	x.setAttribute("type", "range");
-	x.setAttribute("min", min);
-	x.setAttribute("max", max);
-	x.setAttribute("value", val);
+	x.setAttribute("min", arrValues[0]);
+	x.setAttribute("max", arrValues[1]);
+	x.setAttribute("value", arrValues[2]);
     x.className =css;
 	
 	var t = document.createTextNode(title);
-    //document.body.appendChild(t);
+    parent.appendChild(t);
 	
 	parent.appendChild(x);
 	
 	objVal = label(parent,"income");
 	objVal.innerHTML=x.value;
-	
 
-	var evt = $.Event('changed');
-    evt.val = x.value;
-
-   // $(window).trigger(evt);
 	x.onchange=function(){
 		var event = new Event('chang');  // (*)
 		x.dispatchEvent(event);	
@@ -91,14 +163,6 @@ function range(parent,title,css,min,max,val)
 	};
 	return x;
 	
-}
-/*__________________________________________________*/
-function list(parent,css)
-{
-	var x = document.createElement("UL");
-    x.className =css;
-	parent.appendChild(x);
-	return x;
 }
 /*__________________________________________________*/
 function listItem(parent,text,css)
@@ -112,8 +176,10 @@ function listItem(parent,text,css)
 /*__________________________________________________*/
 function Lists(parent,arrItems,css)
 {
-	var  l  = list(parent,css);
-	
+	var l = document.createElement("UL");
+    l.className =css;
+	parent.appendChild(l);
+
 	var len = arrItems.length;
 	for(i=0;i<len;i++)
 		listItem(l,arrItems[i],css);
@@ -129,13 +195,37 @@ function image(parent,source,css)
 	return x;
 }
 /*__________________________________________________*/
-function button(parent,title,css)
+function button(parent,title,onclickFunction,css)
 {
 	var x = document.createElement("INPUT");
 	x.setAttribute("type", "button");
 	x.value= title;
     x.className =css;
+	x.onclick=onclickFunction;
 	parent.appendChild(x);
+	x.onclick=function(){
+		var event = new Event('clicked');  // (*)
+		x.dispatchEvent(event);	
+	};
+	return x;
+}
+/*__________________________________________________*/
+function div(parent,css)
+{
+	var x = document.createElement("div");
+    x.className =css;
+	parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function link(parent,title,url,css)
+{
+	var x = document.createElement("A");
+	x.setAttribute("href", url);
+	x.innerHTML=title;
+    x.className =css;
+	parent.appendChild(x);
+    
 	return x;
 }
 /*__________________________________________________*/
@@ -144,7 +234,7 @@ function label(parent,title,css)
 	var x = document.createElement("DIV");
 	x.innerHTML= title;
     x.className =css;
-	x.style="display:inline-block;";
+	x.style="display:inline-block; ";
 	parent.appendChild(x);
 	return x;
 }
@@ -158,6 +248,38 @@ function checkBox(parent,title,css)
 	parent.appendChild(x);
 	var t = document.createTextNode(title);
     document.body.appendChild(t);
+	return x;
+}
+/*__________________________________________________*/
+function textBox(parent,title,val,css)
+{
+	if(title!="" && title !=undefined)
+	{
+		var t = document.createTextNode(title);
+    	parent.appendChild(t);
+	}
+	var x = document.createElement("INPUT");
+	x.setAttribute("type", "Text");
+	
+	
+    x.className =css;
+	if(val!="" && val !=undefined)
+		x.value=val;
+    parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function dropDown(parent,title,arrItems,css)
+{
+	var x = document.createElement("SELECT");
+	var len=arrItems.length;
+   	for(i=0;i<len;i++)
+	{
+		var option = document.createElement("option");
+		option.text = arrItems[i];
+		x.add(option);
+	}
+	parent.appendChild(x);
 	return x;
 }
 /*__________________________________________________*/
@@ -203,15 +325,6 @@ function simpleDatePicker(parent,title)
 		return x;
 	}
 /*__________________________________________________*/
-function Clear(node)
-{
-
-	while (node.firstChild) {
-		node.removeChild(node.firstChild);
-	}
-	return node;
-}
-/*__________________________________________________*/
 function table(parent,css)
 { 
 	var x = document.createElement("TABLE");
@@ -235,31 +348,52 @@ function tableCell(TableRow,content)
 	
 	if(content!="" && content!=undefined)
 	{
+	
 		var t = document.createTextNode(content);
 		z.appendChild(t);
+		
 	}
     TableRow.appendChild(z);
 	return z;
 }
 /*__________________________________________________*/
-function link(parent,title,url,css)
+function tableRowTextbox(tb,title)
 {
-	var x = document.createElement("A");
-	x.setAttribute("href", url);
-	x.innerHTML=title;
-    x.className =css;
-	parent.appendChild(x);
-    
-	return x;
+	
+	var r = tableRow  ( tb);
+	var c1 = tableCell ( r);
+	label(c1, title);
+	var c2 = tableCell ( r);
+	textBox(c2);
 }
 /*__________________________________________________*/
-function addEvent(element, event, fn) {
-    if (element.addEventListener) {
-        element.addEventListener(event, fn, false);
-    } else {
-        element.attachEvent("on" + event, function() {
-            // set the this pointer same as addEventListener when fn is called
-            return(fn.call(element, window.event));   
-        });
-    }
+function AddHtml2Body(parent,sHtml)
+{
+	
+	parent.insertAdjacentHTML( 'beforeend', sHtml );
+	
+}
+/*__________________________________________________*/
+function Addscript2Head(scriptUrl)
+{
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = scriptUrl;    
+
+	var h=	document.getElementsByTagName('head');
+	if(h.length>0 )
+		h[0].appendChild(script);
+	else
+		document.createElement('head').appendChild(script);
+
+}
+/*__________________________________________________*/
+
+function Clear(node)
+{
+
+	while (node.firstChild) {
+		node.removeChild(node.firstChild);
+	}
+	return node;
 }
