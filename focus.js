dcc04d40c77fc36789fa0ed5c854d71dc94e7c72
Nima <n.habibollahi@yahoo.com>
2017-01-09 17:31:01 +0800

    Add files via upload

---
+++ focus.js
@@ -0,0 +1,24 @@
+
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+var myPrettyCode = function() {
+
+   // Here, do what ever you want
+};
+debugger;
+loadScript("focusBase.js", myPrettyCode);
+loadScript("focusComposite.js", myPrettyCode);
---
+++ focusComposite.js
@@ -0,0 +1,35 @@
+/*__________________________________________________*/	
+function MenuDropdownContainer(parent)
+{
+	var x = document.createElement("DIV");
+	Attribute(x,"style","position: relative; display: inline-block;");
+	parent.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function MenuDropdown(menuContainer,menuTitle)
+{
+	var x=button(menuContainer,menuTitle);
+
+    	var s=s+"padding: 16px;";
+    	s=s+"font-size: 16px;";
+    	s=s+"border: none;";
+    	s=s+"cursor: pointer;";
+	Attribute(x,"style",s);
+	menuContainer.appendChild(x);
+	return x;
+}
+/*__________________________________________________*/
+function subMenu(menu,title,url)
+{
+	var l=link(menu,title,url);
+	
+	var s="display: none;";
+    	s=s+"position: absolute;";
+    	s=s+"background-color: #f9f9f9;";
+    	s=s+"min-width: 160px;";
+    	s=s+"box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);";
+    	Attribute(l,"style",s);
+    	menu.appendChild(l);
+    	return l; 
+}
