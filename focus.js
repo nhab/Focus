3ee8b881b2daa868e497e1a00ca78bea68dd0f77
Nima <n.habibollahi@yahoo.com>
2018-09-22 15:19:19 +0330

    Update focus.js

--- focus.js
+++ focus.js
@@ -1,32 +1,15 @@
-/*
-function loadScript(url, callback)
+function IncludeJS(jsFileName)
 {
-    // Adding the script tag to the head as suggested before
-    var head    = document.getElementsByTagName('head')[0];
-    var script  = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
-    script.onreadystatechange = callback;
-    script.onload = callback;
-
-    // Fire the loading
-    head.appendChild(script);
+    var lib1= document.createElement("script");
+    lib1.src =jsFileName; 
+//debugger;
+/*
+    for (i=0;i< document.getElementsByTagName("head")[0].childNodes.length;i++) 
+        if (document.getElementsByTagName("head")[0].childNodes[i].name == name) { // 1 == Element
+            return;
+        }*/
+    document.getElementsByTagName("head")[0].appendChild(lib1);
 }
-var myPrettyCode = function() {
 
-   // Here, do what ever you want
-};
-debugger;
-loadScript("focusBase.js", myPrettyCode);
-loadScript("focusComposite.js", myPrettyCode);
-*/
-var lib1= document.createElement("script");
-lib1.src = "focusBase.js";  //saved in "other js" folder
-document.getElementsByTagName("head")[0].appendChild(lib1);
-
-var lib2 = document.createElement("script");
-lib2.src = "focusComposite.js";  //saved in "other js" folder
-document.getElementsByTagName("head")[0].appendChild(lib2);
+IncludeJS("focusBase.js");  
+IncludeJS("focusComposite.js");
